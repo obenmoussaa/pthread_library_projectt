02e9487b6a1b17ef97e7b2f0b113cc1469a9b8ae
Medghali majid <majid.medghali@bordeaux-inp.fr>
2024-05-05 19:42:45 +0200

    thread

--- src/thread.c
+++ src/thread.c
@@ -111,7 +111,70 @@ thread_t thread_self(void) {
     return (thread_t) current_thread;
 }
 
+int thread_yield(void) {
+  struct thread *save_head = current_thread;
+  TAILQ_REMOVE(&run_queue, current_thread, queue_threads);
 
+  // si le thread en tete de la file n 'est pas bloqué alors il est prêt à s executer, 
+  // alors current thread doit être préémenté et mis à la fin de la file pour que 1st thread peut s executer
+
+  TAILQ_INSERT_TAIL(&run_queue, current_thread, queue_threads);
+  
+  struct thread *new_current_thread = TAILQ_FIRST(&run_queue);
+  current_thread = new_current_thread;
+  
+  swapcontext(&save_head->uc, &new_current_thread->uc);
+  
+  return 0;
+}
+
+int thread_join(thread_t thread, void **retval) {
+  struct thread *target_thread = (struct thread *) thread;
+  if (!target_thread->finished) {
+    target_thread->waiting_threads = current_thread;
+    struct thread *save_head = TAILQ_FIRST(&run_queue);
+    TAILQ_REMOVE(&run_queue, current_thread, queue_threads);
+    // le thread qui prendra la main est le premier de la file
+    struct thread *new_current_thread = TAILQ_FIRST(&run_queue);
+    current_thread = new_current_thread;
+  
+    swapcontext(&save_head->uc, &new_current_thread->uc);
+  }
+  
+  if (retval != NULL) {
+    *retval = target_thread->ret;
+  }
+
+  return 0;
+}
+
+
+void thread_exit(void *retval) {
+    struct thread *save_head = TAILQ_FIRST(&run_queue);
+    current_thread->finished = 1;
+    current_thread->ret = retval;
+    TAILQ_REMOVE(&run_queue, current_thread, queue_threads);
+    if(current_thread == main_thread){
+      TAILQ_INSERT_HEAD(&dead_queue, current_thread, queue_threads);
+    }
+    else{
+      TAILQ_INSERT_TAIL(&dead_queue, current_thread, queue_threads);
+    }
+
+    if(save_head->waiting_threads != NULL){ 
+      TAILQ_INSERT_HEAD(&run_queue, save_head->waiting_threads, queue_threads);
+    }
+
+    if(!TAILQ_EMPTY(&run_queue)){
+      struct thread *new_current_thread = TAILQ_FIRST(&run_queue);
+      current_thread = new_current_thread;
+      swapcontext(&save_head->uc, &new_current_thread->uc);
+    }
+    else{
+      setcontext(&main_thread->uc);
+    }
+    exit(0);
+}
 
 
 
